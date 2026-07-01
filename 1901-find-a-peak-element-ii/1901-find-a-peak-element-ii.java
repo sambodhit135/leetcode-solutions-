3fb208895f18cfc2728a8141a33e3414b171f208
Sambodhit Chavhan <150767052+sambodhit135@users.noreply.github.com>
2026-07-01 19:40:59 +0530

    Time: 0 ms (100%), Space: 119.4 MB (10.45%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -3,18 +3,39 @@ class Solution {
 
         int m = mat.length;
         int n = mat[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                boolean left = (j == 0) || (mat[i][j] > mat[i][j - 1]);
-                boolean right = (j == n - 1) || (mat[i][j] > mat[i][j + 1]);
-                boolean up = (i == 0) || (mat[i][j] > mat[i - 1][j]);
-                boolean bottom = (i == m - 1) || (mat[i][j] > mat[i + 1][j]);
-
-                if (up && bottom && left && right) {
-                    return new int[] { i, j };
+        int low = 0;
+        int high = m - 1;
+        while (low <= high)
+        {
+            int mid=low+(high-low)/2;
+            int max=-1;
+            int idx=-1;
+            for(int i=0;i<n;i++)
+            {
+                if(mat[mid][i]>max)
+                {
+                    max=mat[mid][i];
+                    idx=i;
                 }
             }
-        }
+            
+            boolean up=mid==0||(max>mat[mid-1][idx]);
+            boolean down=mid==m-1 || (max>mat[mid+1][idx]);
+
+            if(up && down)
+            {
+                return new int[]{mid,idx};
+            }
+            else if(!up)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }            
+        }           
+           
         return new int[] { -1, -1 };
     }
 }

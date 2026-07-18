adc538f2ff9ea5127397663b07e8022fb6c0db21
Sambodhit Chavhan <150767052+sambodhit135@users.noreply.github.com>
2026-07-18 20:12:50 +0530

    Time: 0 ms (100%), Space: 45.3 MB (28.11%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -1,8 +1,14 @@
 class Solution {
     public int findGCD(int[] nums) {
-       mergesort(nums,0,nums.length-1);
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            min=Math.min(min,nums[i]);
+            max=Math.max(max,nums[i]);
+        }
 
-        return gcd(nums[0],nums[nums.length-1]);
+        return gcd(min,max);
     }
 
     public int gcd(int min, int max) {
@@ -15,44 +21,5 @@ class Solution {
         return min;
     }
 
-    public void mergesort(int nums[], int low, int high) {
-        if (low>=high) {
-            return;
-        }
-        int mid = low + (high - low) / 2;
-
-        mergesort(nums, low, mid);
-        mergesort(nums, mid + 1, high);
-        merge(nums, low, mid, high);
-    }
-
-    public void merge(int nums[], int start, int mid, int high) {
-        List<Integer> list = new ArrayList<>();
-        int i = start;
-        int j = mid + 1;
-        while (i <= mid && j <= high) {
-            if (nums[i] <= nums[j]) {
-                list.add(nums[i]);
-                i++;
-            } else {
-                list.add(nums[j]);
-                j++;
-            }
-
-        }
-        while (i <= mid) {
-            list.add(nums[i]);
-            i++;
-        }
-        while (j <= high) {
-            list.add(nums[j]);
-            j++;
-        }
-
-        for( i=start;i<=high;i++)
-        {
-            nums[i]=list.get(i-start);
-        }
-    }
-
+    
 }

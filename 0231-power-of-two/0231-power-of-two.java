07aa5e4f8dc7abb4131792362d2fbef88260e605
Sambodhit Chavhan <150767052+sambodhit135@users.noreply.github.com>
2026-08-24 21:24:00 +0530

    Time: 1 ms (96.62%), Space: 42.4 MB (87.38%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,9 +1,30 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n <= 0) {
+        if(n<0)
+        {
             return false;
         }
+        StringBuilder str=new StringBuilder();
+       while(n!=0)
+       {
+         
+        str.append(n%2);
+        n/=2;
+       }
+       int count=0;
+       for(int i=0;i<str.length();i++)
+       {
+            if(str.charAt(i)=='1')
+            {
+                count++;
+            }
 
-        return (n & (n - 1)) == 0;
+       }
+
+       if(count==1)
+       {
+        return true;
+       }
+       return false;
     }
 }

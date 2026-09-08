6e767da5d6dbac15911df13f03cf2b060d6fa1a4
Sambodhit Chavhan <150767052+sambodhit135@users.noreply.github.com>
2026-09-08 19:49:14 +0530

    Time: 1 ms (83.65%), Space: 42.8 MB (22.48%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countCommas(int n) {
+        if(n<1000)
+        {
+            return 0;
+        }
+        int ans=n-1000;
+        
+        return ans+1;
+
+    }
+}
